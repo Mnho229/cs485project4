a6f85af8300c97cf8236a93194beca40b611ed36
Mnho229 <michaelho3624@gmail.com>
2015-04-01 14:30:30 -0400

    progress in main and helperfunctions

--- helperfunctions.cpp
+++ helperfunctions.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+struct token {
+	string type;
+	string content;
+};
+
+token scanner(string scanned){
+
+}
--- main.cpp
+++ main.cpp
@@ -10,3 +10,19 @@ int main() {
 
 
 }
+
+void parser(string inputline) {
+	vector<token> tokenList;
+
+	while(inputLine != "") {
+		int spacemarker = inputline.find(" \t");
+		string sender = inputline.substr(0, spacemarker);
+		token reply = scanner(sender);
+
+		tokenList.push_back();
+
+		inputLine.erase(0, spacemarker);
+	}
+
+	//syntax
+}
